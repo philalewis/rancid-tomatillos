8c7a9a77430c573aa99f38d4ff0c1468a28da94f
Phil Lewis <phillewis1985@gmail.com>
2022-02-03 14:27:09 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
+import moviesData from './data.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ movies: moviesData.movies })
+  }
+
+  render() {
+    return (
+      <h1>Rancid Tomatillos</h1>
+    )
+  }
 }
 
 export default App;
