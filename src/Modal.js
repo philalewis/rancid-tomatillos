d23d0ea04022b142cd37dc4ee5b579fe65b5fc24
Lynn Nguyen <AmandaN22@outlook.com>
2022-02-05 12:16:58 -0700

    Add styling to error modal

--- src/Modal.js
+++ src/Modal.js
@@ -5,8 +5,8 @@ const Modal = ({ message, exitModal }) => {
   return(
     <section className='modal'>
       <article className='modal-box'>
-        <h3>{message}</h3>
-        <button onClick={exitModal}>OK</button>
+        <h3>Uh Oh! <br/>{message}</h3>
+        <button className='modal-btn' onClick={exitModal} >OK</button>
       </article>  
     </section>
   )
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 const apiCalls = {
   getAllMovies() {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movie`)
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies`)
     .then(response => {
       if (response.ok) {
         return response.json()
@@ -8,18 +8,8 @@ const apiCalls = {
         console.log(response)
         throw `${response.status} ${response.statusText}`
       }
-    //  return response.ok ? 
-    //   response.json() : 
-    //   throw `${response.code} ${response.message}`
     })
-    // .then(result => console.log(result))
-  
   }
-  // checkError(response) {
-  //   if (!response.ok) {
-  //     throw `${response.code} ${response.message}`
-  //   }
-  // }
 }
 
 export default apiCalls;
