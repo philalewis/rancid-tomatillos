f687f3b41f407a82ce634e4a3249575d85e73848
Phil Lewis <phillewis1985@gmail.com>
2022-02-10 15:04:55 -0700

    Write tests for modal

---
+++ cypress/integration/Modal_spec.js
@@ -0,0 +1,61 @@
+describe.only('Modal for error handling', () => {
+
+  it('should display a modal when a network request fails', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+      {
+        statusCode: 404,
+        ok: false,
+      }
+    )
+    cy.visit('http://localhost:3000/')
+      .get('.modal')
+  })
+
+  it('should display different messages for different kinds of errors', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+      {
+        statusCode: 404,
+        ok: false,
+      }
+    )
+    cy.visit('http://localhost:3000/')
+      .get('.modal')
+      .get('h3')
+      .contains('We\'re sorry, something went wrong. Either the page doesn\'t exist, or could not be found')
+
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+      {
+        statusCode: 500,
+        ok: false,
+      }
+    )
+    cy.visit('http://localhost:3000/')
+      .get('.modal')
+      .get('h3')
+      .contains('We\'re sorry, something went wrong with the server. Please try again later')
+  })
+
+  it('should provide a way to exit the modal', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
+      {
+        statusCode: 500,
+        ok: false,
+      }
+    )
+    cy.visit('http://localhost:3000/')
+      .get('.modal-btn')
+      .click()
+      .get('.modal').should('not.exist')
+  })
+
+  it('should be shown an error message anywhere on the site that a network request was made.', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/337401',
+      {
+        statusCode: 500,
+        ok: false,
+      }
+    )
+    cy.visit('http://localhost:3000/337401')
+      .get('.modal')
+  })
+});
